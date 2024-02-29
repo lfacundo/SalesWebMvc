37704001f5190c43d1333d94ab8593c695694d7f
Luiz Facundo <luizg5913@gmail.com>
2024-02-28 21:06:49 -0300

    Seller details and eager loading

--- SalesWebMvc/Models/Department.cs
+++ SalesWebMvc/Models/Department.cs
@@ -16,6 +16,11 @@ namespace SalesWebMvc.Models
         {
         }
 
+        public Department(string name)
+        {
+            Name = name;
+        }
+
         public Department(int id, string name)
         {
             Id = id;
